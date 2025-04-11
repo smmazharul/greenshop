b080b92f48b4ec99f74458516ae1410edfb39818
smmazharul <hmoxygen@gmail.com>
2025-04-11 20:59:09 +0600

    word with detail product page and now will add related product in product details page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import { useAppContext } from './context/AppContext';
 import Login from './components/Login';
 import AllProducts from './pages/AllProducts';
 import ProductCategory from './pages/ProductCategory';
+import ProductDetails from './pages/ProductDetails';
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller")
   const {showUserLogin}=useAppContext()
@@ -23,6 +24,7 @@ const App = () => {
           <Route path='/' element={<Home/>}></Route>
           <Route path='/products' element={<AllProducts/>}></Route>
           <Route path='/products/:category' element={<ProductCategory/>}></Route>
+          <Route path='/products/:category/:id' element={<ProductDetails/>}></Route>
         </Routes>
       </div>
       {!isSellerPath && <Footer/>}
