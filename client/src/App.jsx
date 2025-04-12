20907064b504d820a4e900ea1689fd832a1427a6
smmazharul <hmoxygen@gmail.com>
2025-04-12 13:34:14 +0600

    create a seller component to create seller login form to access the seller dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,9 +12,10 @@ import ProductDetails from './pages/ProductDetails';
 import Cart from './pages/Cart';
 import AddAddress from './pages/AddAddress';
 import MyOrders from './pages/MyOrders';
+import SellerLogin from './components/Seller/SellerLogin';
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller")
-  const {showUserLogin}=useAppContext()
+  const {showUserLogin,isSeller}=useAppContext()
   return (
     <div>
       {isSellerPath ? null: <Navbar></Navbar> }
@@ -31,6 +32,9 @@ const App = () => {
           <Route path='/cart' element={<Cart/>}></Route>
           <Route path='/add-address' element={<AddAddress/>}></Route>
           <Route path='/my-orders' element={<MyOrders/>}></Route>
+          <Route path='/seller' element={isSeller?null:<SellerLogin/>}>
+              
+          </Route>
         </Routes>
       </div>
       {!isSellerPath && <Footer/>}
