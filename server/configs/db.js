7793373414f9443ac6b4cb8fa688de34b8fd7244
smmazharul <hmoxygen@gmail.com>
2025-04-14 13:44:59 +0600

    import connect db from config file

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
-const conncetDB =async()=>{
+const connectDB =async()=>{
     try {
         mongoose.connect.on('connected',()=>console.log("Database Connected")
         );
         await mongoose.connect(`${process.env.MONGODB_URI}/greenshop`)
 
     } catch (error) {
-        console.log(error,message);
+        console.log(error.message);
     }
 }
+
+export default connectDB;
