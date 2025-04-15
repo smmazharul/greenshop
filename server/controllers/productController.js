92b5d1b330dafa2d5981e566fb9e0ce27d1cbed4
smmazharul <hmoxygen@gmail.com>
2025-04-16 00:17:55 +0600

    create a funtion get single product using ID in product controller

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -42,7 +42,14 @@ export const productList = async(req,res)=>{
 
 // Get single Product : /api/product/id
 export const productById = async(req,res)=>{
-
+    try {
+        const {id}= req.body;
+        const product = await Product.findById(id)
+        res.json({success:true,product})
+    } catch (error) {
+        console.log(error.message)
+        res.json({success:false,message:error.message});
+    }
 }
 
 
