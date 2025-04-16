73e598cf83ff2ecf41c1ebc433898192de09ddcd
smmazharul <hmoxygen@gmail.com>
2025-04-16 11:39:34 +0600

    create cart router for updating the data

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import bodyParser from 'body-parser';
 import sellerRouter from './routes/sellerRoute.js';
 import connectCloudinary from './configs/cloudinary.js';
 import productRouter from './routes/productRoute.js';
+import cartRouter from './routes/cartRoute.js';
 
 const app = express();
 
@@ -36,6 +37,7 @@ app.get('/',(req,res)=>{
 app.use('/api/user',userRouter)
 app.use('/api/seller',sellerRouter)
 app.use('/api/product',productRouter)
+app.use('/api/cart',cartRouter)
 
 
 app.listen(port,()=>{
