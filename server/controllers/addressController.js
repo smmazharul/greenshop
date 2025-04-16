9fae9d1aa6cfe9a423adb34a2da9c8a8759c9544
smmazharul <hmoxygen@gmail.com>
2025-04-16 11:50:41 +0600

    create address controller to create address

---
+++ server/controllers/addressController.js
@@ -0,0 +1,17 @@
+
+// Add Address : /api/address/add
+
+import Address from "../models/Address.js";
+
+export const addAddress=async(req,res)=>{
+    try {
+        const {address,userId}=req.body;
+        await Address.create({...address,userId})
+        res.json({success:true , message:"Address added successfully  "})
+
+
+    } catch (error) {
+        console.log(error.message)
+        res.json({success:false,message:error.message});
+    }
+}
