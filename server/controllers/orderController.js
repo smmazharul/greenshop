9868a295637ef58bc18e44817ed15618ec352342
smmazharul <hmoxygen@gmail.com>
2025-04-16 14:42:02 +0600

    create new function to get all order for admin

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -46,3 +46,15 @@ export const getUserOrders =async(req,res)=>{
         res.json({success:false,message:error.message});
     }
 }
+
+// get all order (for seller /admin): /api/order/seller
+export const getAllOrders =async(req,res)=>{
+    try {
+        const orders = await Order.find({ $or:[{paymentType:"COD"},{isPaid:true}]
+        }).populate("items.product address");
+        res.json({success:true, orders})
+    } catch (error) {
+        console.log(error.message)
+        res.json({success:false,message:error.message});
+    }
+}
