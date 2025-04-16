44b182bffe1e53db829595985b102f46b8e53745
smmazharul <hmoxygen@gmail.com>
2025-04-16 14:39:34 +0600

    create new function to get user Id means single user data

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -31,3 +31,18 @@ export const placeOrderCOD = async(req,res)=>{
         res.json({success:false,message:error.message});
     }
 }
+
+
+// get order by user ID: /api/order/user
+
+export const getUserOrders =async(req,res)=>{
+    try {
+        const {userId} =req.body;
+        const orders = await Order.find({userId,$or:[{paymentType:"COD"},{isPaid:true}]
+        }).populate("items.product address").sort({createdAt:-1});
+        res.json({success:true, orders})
+    } catch (error) {
+        console.log(error.message)
+        res.json({success:false,message:error.message});
+    }
+}
