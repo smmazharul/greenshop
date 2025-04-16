70a1f638a80ac1ef70f05399793819778ad02621
smmazharul <hmoxygen@gmail.com>
2025-04-16 11:53:59 +0600

    create address get fucntion

--- server/controllers/addressController.js
+++ server/controllers/addressController.js
@@ -15,3 +15,17 @@ export const addAddress=async(req,res)=>{
         res.json({success:false,message:error.message});
     }
 }
+
+// get Address : /api/address/get
+
+export const getAddress=async(req,res)=>{
+    try {
+        const {userId}=req.body;
+        const addresses = await Address.find({userId})
+        res.json({success:true , addresses})
+
+    } catch (error) {
+        console.log(error.message)
+        res.json({success:false,message:error.message});
+    }
+}
