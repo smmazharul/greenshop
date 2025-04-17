48db3b3ac18c9d9df77f726e84532d41307716ff
smmazharul <hmoxygen@gmail.com>
2025-04-17 23:42:08 +0600

    seller logout set in frontend

--- client/src/pages/seller/SellerLayout.jsx
+++ client/src/pages/seller/SellerLayout.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { useAppContext } from "../../context/AppContext";
 import { assets } from "../../assets/assets";
 import { Link, NavLink, Outlet } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const SellerLayout = () => {
-  const { setIsSeller } = useAppContext();
+  const { axios,navigate} = useAppContext();
 
   const sidebarLinks = [
     { name: "Add Product", path: "/seller", icon: assets.add_icon },
@@ -18,7 +19,19 @@ const SellerLayout = () => {
 
 
   const logout =async ()=>{
-    setIsSeller(false)
+    // setIsSeller(false)
+    try {
+      const {data} =await axios.get('api/seller/logout')
+      if(data.success){
+        toast.success(data.message)
+        navigate('/')
+        
+      }else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
   }
 
 
