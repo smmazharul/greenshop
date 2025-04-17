229c242c408441779ffdb89223356cfcb5f54bab
smmazharul <hmoxygen@gmail.com>
2025-04-18 00:23:10 +0600

    to post product data from seller

--- client/src/pages/seller/AddProduct.jsx
+++ client/src/pages/seller/AddProduct.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { assets, categories } from '../../assets/assets';
-
+import { useAppContext } from "../context/AppContext";
+import toast from 'react-hot-toast';
 const AddProduct = () => {
 
     const [files,setFiles]=useState([])
@@ -10,8 +11,42 @@ const AddProduct = () => {
     const [price ,setPrice]=useState('');
     const [offersPrice ,setOffersPrice]=useState('');
 
+    const {axios} = useAppContext
+
+
     const onSubmitHandler=async(event)=>{
-        event.preventDefault();
+        try {
+            event.preventDefault();
+            const productData ={
+                name,
+                description:description.split('\n'),
+                category,
+                price,
+                offersPrice
+            }
+
+            const formData = new FormData();
+            formData.append('productData',JSON.stringify(productData));
+            for (let i = 0; i < files.length; i++) {
+                formData.append('images',files[i])
+                
+            }
+            const {data} =axios.post('/api/product/add',formData)
+            if(data.success){
+                toast.success(data.message);
+                setName('')
+                setDescription('')
+                setCategory('')
+                setPrice('')
+                setOffersPrice('')
+                setFiles([])
+            }else{
+                toast.error(data.message)
+            }
+            
+        } catch (error) {
+            toast.error(error.message)
+        }
     } 
 
 
