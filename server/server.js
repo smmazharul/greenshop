8de8cfbdb5574a6d68060ccaad9ed7464522c07b
smmazharul <hmoxygen@gmail.com>
2025-04-18 17:47:51 +0600

    set the stripe route

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ import productRouter from './routes/productRoute.js';
 import cartRouter from './routes/cartRoute.js';
 import addressRouter from './routes/addressRoute.js';
 import orderRouter from './routes/orderRoute.js';
+import { stripeWebhooks } from './controllers/orderController.js';
 
 const app = express();
 
@@ -21,6 +22,8 @@ await connectCloudinary()
 
 // Allowed multiple origins 
 const allowedOrigin = ['http://localhost:5173']
+
+app.post('/stripe',express.raw({type:'application/json'}),stripeWebhooks)
 
 // Middleware configuration
 app.use(express.json());
